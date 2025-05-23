bc139ec256554140e6face5f548794e6b0738008
GangHyoun <tail6622@naver.com>
2025-05-23 14:08:42 +0900

    Update : 프론트 url application.properties로 변경

--- src/main/java/org/example/purgo_chat/config/WebConfig.java
+++ src/main/java/org/example/purgo_chat/config/WebConfig.java
@@ -1,15 +1,20 @@
 package org.example.purgo_chat.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("frontendUrl")
                 // 3000 포트에서만 허용 권장
                 .allowedMethods("GET") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
